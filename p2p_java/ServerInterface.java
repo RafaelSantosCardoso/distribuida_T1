fd6c22f268b891cc228094bcdca0d91075e89cb0
rafael <rafaelcardoso2604@gmail.com>
2022-04-18 00:33:46 -0300

    vai

--- p2p_java/ServerInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.*;
-
-public interface ServerInterface extends Remote {
-    public int register(int port) throws RemoteException;
-    public int end(int id) throws RemoteException;
-}
--- p2p_java/p2pPeer.java
+++ p2p_java/p2pPeer.java
@@ -5,17 +5,24 @@ import java.util.*;
 public class p2pPeer {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length != 3) {
-			System.out.println("Uso: java p2pPeer <server> \"<message>\" <localport>");
+		if (args.length != 2) {
+			System.out.println("Uso: java p2pPeer server <serverip>");
 			System.out.println("<message> is:");
 			System.out.println("create nickname");
 			System.out.println("list nickname");
 			System.out.println("wait");
 			return;
 		} else {
-			new p2pPeerThread(args).start();
-			new p2pPeerHeartbeat(args).start();
-			new p2pPeerClient(args).start();
+			switch(args[0]){
+                case "server":
+                    new p2pServer(args).start();
+                    break;
+                case "peer":
+                    new p2pPeerThread(args).start();
+			        new p2pPeerHeartbeat(args).start();
+			        new p2pPeerClient(args).start();
+                    break;
+		    }
 		}
 	}
 }
